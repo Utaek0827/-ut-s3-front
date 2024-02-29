56c792361cd87c593bcc5d84e37af475fdda29c2
utaek_mac <utaek777@gmail.com>
2024-02-29 16:46:05 +0900

    백엔드연동

---
+++ src/setupProxy.js
@@ -0,0 +1,26 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+//   app.use('/api',
+//     createProxyMiddleware({
+//       // target: 'https://utapi.duckdns.org',
+
+//       // target: 'http://uutt77.duckdns.org:10030',
+//       target: 'http://localhost:8080/',
+
+//       changeOrigin: true,
+//     }),
+//   );
+
+  app.use('/auth',
+    createProxyMiddleware({
+      // target: 'https://utapi.duckdns.org',
+
+      // target: 'http://uutt77.duckdns.org:10030',
+      target: 'http://localhost:8080/api/v1',
+
+      changeOrigin: true,
+    }),
+  );
+  
+};

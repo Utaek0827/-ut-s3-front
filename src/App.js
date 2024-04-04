b920191fe3ef27b60bdf28e9192e87370b415224
utaek_mac <utaek777@gmail.com>
2024-04-04 16:58:32 +0900

    json  대시보드에서 받기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import SignIn from './auth/SignIn';
 import SignUp from './auth/SignUp';
 import Dashboard from './dashboard/Dashboard';
+import Orders from './dashboard/Orders';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<SignIn /> }/>
         <Route path='/SignUp' element={<SignUp />}/>
         <Route path='/Dashboard' element={<Dashboard />}/>
+        <Route path='/Orders' element={<Orders />}/>
 
       </Routes>
     </div>
